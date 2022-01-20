08f5dcdf07f896b20c59ab4afca3e7416f8fea13
rhjshi <rhjshi@uwaterloo.ca>
2022-01-20 14:16:57 -0500

    post ticket endpoint works and unmarshalls

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
+// https://tutorialedge.net/golang/creating-restful-api-with-golang/
+// https://gobyexample.com/json
+
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+// Fields need to be exported, by capitalizing, in order for json module to access
+// json tags used for mapping json obj field to struct field
 type Ticket struct {
-	title  string
-	points int
-	owner  string
+	Title  string `json:"title"`
+	Points int    `json:"points"`
+	Owner  string `json:"owner"`
 }
 
 func main() {
+	fmt.Println("starting simpleapi")
+
 	r := mux.NewRouter()
 	r.StrictSlash(true)
 
@@ -26,7 +35,18 @@ func main() {
 }
 
 func TicketPostHandler(rw http.ResponseWriter, req *http.Request) {
+	reqBody, _ := ioutil.ReadAll(req.Body)
+	fmt.Printf("Handling ticket POST\n%+v\n", string(reqBody))
 
+	var ticket Ticket
+	err := json.Unmarshal(reqBody, &ticket)
+
+	if err != nil {
+		fmt.Println("Error parsing json object:", err)
+		return
+	}
+
+	fmt.Println(ticket)
 }
 
 func HomeHandler(rw http.ResponseWriter, req *http.Request) {
