7224a24510df9853e3d5d0af913f17b4c264d8e4
rhjshi <rhjshi@uwaterloo.ca>
2022-01-20 12:59:57 -0500

    basic api setup

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -14,13 +15,20 @@ type Ticket struct {
 }
 
 func main() {
-	fmt.Println("Hello World!")
-
 	r := mux.NewRouter()
+	r.StrictSlash(true)
 
+	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/ticket", TicketPostHandler).Methods("POST")
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func TicketPostHandler(rw http.ResponseWriter, req *http.Request) {
 
+}
+
+func HomeHandler(rw http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(rw, "Hello World!")
 }
