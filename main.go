3a5acb45f95e502f0f19f992757963a148a46d3c
rhjshi <rhjshi@uwaterloo.ca>
2022-01-20 11:53:12 -0500

    init mux router and types

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+type Ticket struct {
+	title  string
+	points int
+	owner  string
+}
 
 func main() {
 	fmt.Println("Hello World!")
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/ticket", TicketPostHandler).Methods("POST")
+}
+
+func TicketPostHandler(rw http.ResponseWriter, req *http.Request) {
+
 }
